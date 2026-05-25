204b2ef007d6292ef871adf5f89919a6d85c2934
AKshita <gupta.akshita@codiotic.com>
2026-05-25 16:46:56 +0530

    fix: stripUnknown false

--- reviwee-api/api/v1/middleware/validate.js
+++ reviwee-api/api/v1/middleware/validate.js
@@ -1,22 +1,24 @@
 const Joi = require("joi");
 const httpStatus = require("http-status");
 const pick = require("../../../utilities/pick");
-const ApiError = require("../../../utilities/apiErrorUtils");
 
 const validate = (schema) => (req, res, next) => {
   const validSchema = pick(schema, ["params", "query", "body"]);
   const object = pick(req, Object.keys(validSchema));
   const { value, error } = Joi.compile(validSchema)
-    .prefs({ errors: { label: "key" }, abortEarly: false })
+    .prefs({ 
+      errors: { label: "key" }, 
+      abortEarly: false, 
+      allowUnknown: true,
+      stripUnknown: false 
+    })
     .validate(object);
 
   if (error) {
     let errorMsg = [];
-    const errorMessage = error.details.map((details) => {
-      errorMsg.push(details.message.replace(/"/g, `'`));
-      details.message;
+    error.details.map((d) => {
+      errorMsg.push(d.message.replace(/"/g, "'"));
     });
-
     return res.status(httpStatus.BAD_REQUEST).send({
       message: errorMsg.toString(),
       status: false,
@@ -24,11 +26,11 @@ const validate = (schema) => (req, res, next) => {
       code: "INVALID_DATA",
       issue: "INVALID_DATA",
     });
-    // return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
-req.body = Object.assign({}, req.body, value.body || {});
-req.params = Object.assign({}, req.params, value.params || {});
-req.query = Object.assign({}, req.query, value.query || {});
+
+  req.body = Object.assign({}, req.body, value.body || {});
+  req.params = Object.assign({}, req.params, value.params || {});
+  req.query = Object.assign({}, req.query, value.query || {});
   return next();
 };
 
